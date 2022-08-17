a6b60e5618dc3a38ee213892af99bdca8db0494e
Karim-Omran-Elatbany <95541499+Karim-Omran-Elatbany@users.noreply.github.com>
2022-08-17 11:20:32 -0700

    Update HomeScreen.py

--- HomeScreen.py
+++ HomeScreen.py
@@ -9,10 +9,10 @@ x = int((screenwidth - width)/2)
 y = int((screenheight - height)/2)
 pro.geometry(f"{width}x{height}+{x}+{y}")
 pro.resizable(False,False)
-pro.iconbitmap('D:\AMR\python project\\New folder\\Restaurant\\Images\\restaurant.ico')
-photo=PhotoImage(file='D:\AMR\python project\\New folder\\Restaurant\\Images\\HomeScreen.png')
-panel=Label(pro, image=photo)
-panel.pack()
+#pro.iconbitmap('D:\AMR\python project\\New folder\\Restaurant\\Images\\restaurant.ico')
+#photo=PhotoImage(file='D:\AMR\python project\\New folder\\Restaurant\\Images\\HomeScreen.png')
+#panel=Label(pro, image=photo)
+#panel.pack()
 def manager():
     pro.destroy()
     import History
@@ -20,7 +20,7 @@ bt1=Button(pro,text='Manager',font=('courier',15,"bold"),bg='black',fg='white',w
 bt1.place(x=60,y=250)
 def User():
     pro.destroy()
-    import Home
+   import Menu
 bt2=Button(pro,text='User',font=('courier',15,"bold"),bg='black',fg='white',width=10,height=1,command=User)
 bt2.place(x=300,y=250)
 

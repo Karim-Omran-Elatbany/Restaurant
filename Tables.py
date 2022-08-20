b14aa25f57c18d9309062d9f1256a6b9492d3fa1
‪Karim Ehab‬‏ <87093504+KarimEhab1@users.noreply.github.com>
2022-08-20 03:48:51 +0200

    Update Tables.py

--- Tables.py
+++ Tables.py
@@ -24,59 +24,65 @@ var4_For_Tables = BooleanVar()
 var5_For_Tables = BooleanVar()
 var6_For_Tables = BooleanVar()
 
-#--------this 
-photo=PhotoImage(file='E:\\Restaurant-main\\Images\\k.png')
-panel = Label(yy, image=photo)
-panel.place(x=10,y=50)
+#--------this
+# photo=PhotoImage(file='E:\\Restaurant-main\\Images\\k.png')
+# panel = Label(yy, image=photo)
+# panel.place(x=10,y=50)
 
-c1_For_Tables = Checkbutton(yy, text=' Single 1', variable=var1_For_Tables)
-c1_For_Tables.deselect()
-c1_For_Tables.place(x=10, y=170)
+Table1 = Checkbutton(yy, text=' Single ', variable=var1_For_Tables)
+Table1.deselect()
+Table1.place(x=10, y=170)
 
-photo2=PhotoImage(file='E:\\Restaurant-main\\Images\\k.png')
-pane2 = Label(yy, image=photo)
-pane2.place(x=190,y=50)
+# photo2=PhotoImage(file='E:\\Restaurant-main\\Images\\k.png')
+# pane2 = Label(yy, image=photo)
+# pane2.place(x=190,y=50)
 
-c2_For_Tables = Checkbutton(yy, text=' Double 2', variable=var2_For_Tables)
-c2_For_Tables.deselect()
-c2_For_Tables.place(x=190, y=170)
+Table2 = Checkbutton(yy, text=' couple ', variable=var2_For_Tables)
+Table2.deselect()
+Table2.place(x=190, y=170)
 
-photo3=PhotoImage(file='E:\\Restaurant-main\\Images\\k.png')
-pane3 = Label(yy, image=photo)
-pane3.place(x=370,y=50)
+# photo3=PhotoImage(file='E:\\Restaurant-main\\Images\\k.png')
+# pane3 = Label(yy, image=photo)
+# pane3.place(x=370,y=50)
 
-c3_For_Tables = Checkbutton(yy, text=' Friends 4', variable=var3_For_Tables)
-c3_For_Tables.deselect()
-c3_For_Tables.place(x=370, y=170)
+Table3 = Checkbutton(yy, text=' 4 Friends ', variable=var3_For_Tables)
+Table3.deselect()
+Table3.place(x=370, y=170)
 
-photo4=PhotoImage(file='E:\\Restaurant-main\\Images\\k.png')
-pane4 = Label(yy, image=photo)
-pane4.place(x=10,y=250)
+# photo4=PhotoImage(file='E:\\Restaurant-main\\Images\\k.png')
+# pane4 = Label(yy, image=photo)
+# pane4.place(x=10,y=250)
 
-c4_For_Tables = Checkbutton(yy, text='Family 4', variable=var4_For_Tables)
-c4_For_Tables.deselect()
-c4_For_Tables.place(x=10, y=360)
+Table4 = Checkbutton(yy, text='Family ', variable=var4_For_Tables)
+Table4.deselect()
+Table4.place(x=10, y=360)
 
-photo5=PhotoImage(file='E:\\Restaurant-main\\Images\\k.png')
-pane5 = Label(yy, image=photo)
-pane5.place(x=190,y=250)
+# photo5=PhotoImage(file='E:\\Restaurant-main\\Images\\k.png')
+# pane5 = Label(yy, image=photo)
+# pane5.place(x=190,y=250)
 
-c5_For_Tables = Checkbutton(yy, text='Friends 6', variable=var5_For_Tables)
-c5_For_Tables.deselect()
-c5_For_Tables.place(x=190, y=360)
+Table5 = Checkbutton(yy, text='6 Friends', variable=var5_For_Tables)
+Table5.deselect()
+Table5.place(x=190, y=360)
 
-photo6=PhotoImage(file='E:\\Restaurant-main\\Images\\k.png')
-pane6 = Label(yy, image=photo)
-pane6.place(x=370,y=250)
+# photo6=PhotoImage(file='E:\\Restaurant-main\\Images\\k.png')
+# pane6 = Label(yy, image=photo)
+# pane6.place(x=370,y=250)
 
-c6_For_Tables = Checkbutton(yy, text='Family 6', variable=var6_For_Tables)
-c6_For_Tables.deselect()
-c6_For_Tables.place(x=370, y=360)
+Table6 = Checkbutton(yy, text='8 friends', variable=var6_For_Tables)
+Table6.deselect()
+Table6.place(x=370, y=360)
+
 
 #-------Bottom---------
 def submit():
-    yy.destroy()
-    import Menu
+    if var2_For_Tables.get():
+        tkinter.messagebox.showerror('System', 'Table 2 is already reserved ')
+    elif var4_For_Tables.get():
+        tkinter.messagebox.showerror('System', 'Table 4 is already reserved ')
+    else:
+        yy.destroy()
+        import main_project
 
 
 def back():
